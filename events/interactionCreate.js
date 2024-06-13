ebe36d4b8fdd11e75a5decb69ef4985799ab0f67
hiradko135 <hiradkolahi69@gmail.com>
2024-06-13 00:57:17 -0400

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,80 +1,27 @@
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
-
-
 const config = require("../config.js");
-const { InteractionType } = require('discord.js');
-const fs = require("fs");
-
-module.exports = async (client, interaction) => {
-  try {
-    if (!interaction?.guild) {
-      return interaction?.reply({ content: "This command can only be used in a server.", ephemeral: true });
-    } else {
-      function cmd_loader() {
-        if (interaction?.type === InteractionType.ApplicationCommand) {
-          fs.readdir(config.commandsDir, (err, files) => {
-            if (err) throw err;
-            files.forEach(async (f) => {
-              let props = require(`.${config.commandsDir}/${f}`);
-              if (interaction.commandName === props.name) {
-                try {
-                  if (interaction?.member?.permissions?.has(props?.permissions || "0x0000000000000800")) {
-                    return props.run(client, interaction);
-                  } else {
-                    return interaction?.reply({ content: `You don't have permission to use this command.`, ephemeral: true });
-                  }
-                } catch (e) {
-                  return interaction?.reply({ content: `❌ Error: ${e.message}`, ephemeral: true });
-                }
-              }
-            });
-          });
-        }
-      }
-
-      cmd_loader();
-    }
-  } catch (e) {
-    console.error(e);
-  }
+const { ActivityType  } = require("discord.js")
+module.exports = async (client) => {
+  
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
+(async () => {
+try {
+await rest.put(Routes.applicationCommands(client.user.id), {
+body: await client.commands,
+});
+console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded successfully!')
+} catch (err) {
+console.log('\x1b[36m%s\x1b[0m', '|    ❌ Commands Failed To Load!');
 }
-/*
+})();
 
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
+console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
-
+const serverCount = client.guilds.cache.size;
+setInterval(() => client.user.setActivity({ 
+  name:`lavaLink GlaceYT`, 
+  type: ActivityType.Listening }), 10000);
+client.errorLog = config.errorLog
+  
+}
